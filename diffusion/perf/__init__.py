fdc0b5350e3fb8359aedbd42dcf71376157645fa
frosty <129897719+Makobcki@users.noreply.github.com>
2026-05-03 15:10:28 +0300

    perf: add static MMDiT cost profile

--- diffusion/perf/__init__.py
+++ diffusion/perf/__init__.py
@@ -1,3 +1,4 @@
 from .perf import PerfConfig, configure_performance
+from .model_cost import ModelCostProfile, build_model_cost_profile
 
-__all__ = ["PerfConfig", "configure_performance"]
+__all__ = ["PerfConfig", "configure_performance", "ModelCostProfile", "build_model_cost_profile"]
